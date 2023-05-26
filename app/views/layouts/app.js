f6e42cc1b1151ca60dd97ecf60bc0f7a013c1d9d
sonodap <t106r61@gmail.com>
2023-05-26 14:58:36 +0000

    javascript2章ー四則演算の書き方

--- app/views/layouts/app.js
+++ app/views/layouts/app.js
@@ -1,8 +1,8 @@
-let hello = 'Hello World';
+let hello = 'Hello World ';
 
 let drink = 'coke';
 
 let int = '-10';
 
-// ()の部分を入れ替えるだけで代入できる文字が変わる
-alert(int);
+// ()の部分を入れ替えるだけで代入できる文字が変わる,足し算、引き算、掛け算、割り算もできる
+alert(hello + drink);
