7b5ccba287423fba02a94b961b260297170c97c4
sonodap <t106r61@gmail.com>
2023-05-27 16:43:04 +0000

    javascript2章ー条件分岐の書き方ver2

--- app/views/layouts/app.js
+++ app/views/layouts/app.js
@@ -1,12 +1,14 @@
+// ()の部分を入れ替えるだけで代入できる文字が変わる,足し算、引き算、掛け算、割り算もできる
+// alert(hello + drink);
+
+
 let hello = 'Hello World ';
 
 let drink = 'coke';
 
 let int = '-10';
 
-
-// ()の部分を入れ替えるだけで代入できる文字が変わる,足し算、引き算、掛け算、割り算もできる
-// alert(hello + drink);
+// orangeとappleの数字を変えると帰ってくる言葉も変わってくる
 
 let orange = 100;
 let apple = 90;
