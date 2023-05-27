0738ad522c2b4550a7a3101a65e12dcbe7fda540
sonodap <t106r61@gmail.com>
2023-05-27 16:51:50 +0000

    while文の書き方

--- app/views/layouts/app.js
+++ app/views/layouts/app.js
@@ -8,15 +8,29 @@ let drink = 'coke';
 
 let int = '-10';
 
+
 // orangeとappleの数字を変えると帰ってくる言葉も変わってくる
+// let orange = 100;
+// let apple = 90;
 
-let orange = 100;
-let apple = 90;
+// if(orange < apple){
+//   alert('みかんの値段がリンゴより安い');
+// } else if(orange == apple){
+//   alert('みかんとリンゴが同じ値段');
+// } else{
+//   alert('みかんの値段がリンゴより高い');
+// }
 
-if(orange < apple){
-  alert('みかんの値段がリンゴより安い');
-} else if(orange == apple){
-  alert('みかんとリンゴが同じ値段');
-} else{
-  alert('みかんの値段がリンゴより高い');
+
+// whileは条件式がtrueになるまで繰り返し処理される
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
 }
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です')
