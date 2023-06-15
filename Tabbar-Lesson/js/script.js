d137b6ab66af28a920447d3868b6f4d63b163771
sonodap <t106r61@gmail.com>
2023-06-15 07:07:15 +0000

    script.jsの記述完了

--- Tabbar-Lesson/js/script.js
+++ Tabbar-Lesson/js/script.js
@@ -1,9 +1,9 @@
-$(function() {
-  $('.menu-trigger').on('click', function(event) {
-     // .menu-triggerクリック時に行われる処理
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
-  });
-});
+$('#tab-contents .tab[id !="tab1"]').hide();
 
+$('tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
+});
